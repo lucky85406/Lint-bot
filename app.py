de1b7ba31dda9c9ab460953740d5189db7db319c
陳柏任 <lucky85406@gmail.com>
2019-11-04 06:43:52 +0800

    make it better

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
+    	if event.message.text == "132":
+    		print event.message.text,
         event.reply_token,
  		TextSendMessage(text=event.message.text))
     	
