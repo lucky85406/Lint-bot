ea0c7cf14990f462e5c8f8fe872172077f291676
陳柏任 <lucky85406@gmail.com>
2019-11-04 06:28:49 +0800

    make it better

--- app.py
+++ app.py
@@ -45,8 +45,9 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="繼續努力!!")),
-    	if "a" == "a":TextSendMessage(text="好好加油!!"))
+        TextSendMessage(text="繼續努力!!"),
+    	if "a" == "a":
+    		TextSendMessage(text="好好加油!!"))
     	
 
 
