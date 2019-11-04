31fa71074ed15e2fda087debbc9095b9ac458b6f
陳柏任 <lucky85406@gmail.com>
2019-11-04 21:23:07 +0800

    make it better

--- app.py
+++ app.py
@@ -56,11 +56,12 @@ def function(event):
 	pacelist = [["o","家樂福"]]
 	pricelist = [["p","351~600"],["q","100~350"],["r","600+"]]
 
-	drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"],["afkop","冰靈麗絲玲黑皮諾微甜粉紅酒","黃尾袋鼠粉紅氣泡酒","邁坡莊園花漾微甜粉紅酒","南法小豬格納希粉紅酒"]]
+	drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"]\
+				,["afkop","冰靈麗絲玲黑皮諾微甜粉紅酒","黃尾袋鼠粉紅氣泡酒","邁坡莊園花漾微甜粉紅酒","南法小豬格納希粉紅酒"]]
 	addstr=''
 	for y in range(0,2):
 		if event.message.text == (drinklist[y][0]):
-			for x in range(1,7):
+			for x in range(1,len(drinklist[y])):
 				addstr += drinklist[y][x]+"\n"
 			line_bot_api.reply_message(	
         		event.reply_token,
