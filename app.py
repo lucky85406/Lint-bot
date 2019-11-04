57e632c9eebde4817f8f2e87f889964cdc155991
陳柏任 <lucky85406@gmail.com>
2019-11-04 20:51:05 +0800

    make it better

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def function(event):
 	drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"]]
 	addstr=''
 	for x in range(1,6):
-		addstr += drinklist[0][x]+" "
+		addstr += drinklist[0][x]+"\n"
 	if event.message.text == (drinklist[0][0]):
 			line_bot_api.reply_message(	
         		event.reply_token,
