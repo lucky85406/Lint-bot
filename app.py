22ca681d67ca7e5f9d429674e088f7535b8cd7fc
陳柏任 <lucky85406@gmail.com>
2019-11-04 20:56:01 +0800

    make it better

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusern
 cursor = conn.cursor()  
 cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")  
 row = cursor.fetchone()  
-while row:  
-        print "Inserted Product ID : " +str(row[0])  
-        row = cursor.fetchone()  
+while row:
+	print "Inserted Product ID : " +str(row[0])  
+	row = cursor.fetchone()  
 conn.commit()
 conn.close()
 
