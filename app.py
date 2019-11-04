b7f959d15222dc370f8918ffe8b9e8ab693346f7
陳柏任 <lucky85406@gmail.com>
2019-11-05 00:20:03 +0800

    make it better

--- app.py
+++ app.py
@@ -54,7 +54,11 @@ def function(event):
     original_content_url='https://i.screenshot.net/eqdppak',
     preview_image_url='https://i.screenshot.net/eqdppak'
 	)
-	line_bot_api.reply_message(event.reply_token, message)
+	message2 = ImageSendMessage(
+    original_content_url='https://i.screenshot.net/eqdppak',
+    preview_image_url='https://i.screenshot.net/eqdppak'
+	)
+	line_bot_api.reply_message(event.reply_token, message2)
 
 	#foodlist = [["a","白肉"], ["b","紅肉"],["c","海鮮"],["d","甜點"]]
 	#aromalist = [["e","花香"], ["f","漿果"],["g","柑橘"],["h","熱帶水果"],["i","淺色水果"],["j","香料"]]
