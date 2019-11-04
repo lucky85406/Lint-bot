305b323f673fcd254b715c279362b377b145e0a1
陳柏任 <lucky85406@gmail.com>
2019-11-04 21:45:04 +0800

    make it better

--- app.py
+++ app.py
@@ -59,13 +59,17 @@ def function(event):
 	drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"]\
 				,["afkop","冰靈麗絲玲黑皮諾微甜粉紅酒","黃尾袋鼠粉紅氣泡酒","邁坡莊園花漾微甜粉紅酒","南法小豬格納希粉紅酒"]]
 	addstr=''
+
+	message2 = ImageSendMessage(original_content_url='https://example.com/original.jpg',
+    						preview_image_url='https://example.com/preview.jpg')
+
 	for y in range(0,2):
 		if event.message.text == (drinklist[y][0]):
 			for x in range(1,len(drinklist[y])):
 				addstr += drinklist[y][x]+"\n"
 			line_bot_api.reply_message(	
         		event.reply_token,
-        		TextSendMessage(text="your choose->\n"+addstr))
+        		TextSendMessage(text="your choose->\n"+addstr),message2)
 
 		
 
