cf7f90b06fd72db418dda06a98ee68b26c83b4ad
陳柏任 <lucky85406@gmail.com>
2019-11-04 21:53:22 +0800

    make it better

--- app.py
+++ app.py
@@ -50,25 +50,47 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
-	foodlist = [["a","白肉"], ["b","紅肉"],["c","海鮮"],["d","甜點"]]
-	aromalist = [["e","花香"], ["f","漿果"],["g","柑橘"],["h","熱帶水果"],["i","淺色水果"],["j","香料"]]
-	tastelist = [["k","清香"], ["l","甜"],["m","圓潤"],["n","濃厚"]]
-	pacelist = [["o","家樂福"]]
-	pricelist = [["p","351~600"],["q","100~350"],["r","600+"]]
+	#foodlist = [["a","白肉"], ["b","紅肉"],["c","海鮮"],["d","甜點"]]
+	#aromalist = [["e","花香"], ["f","漿果"],["g","柑橘"],["h","熱帶水果"],["i","淺色水果"],["j","香料"]]
+	#tastelist = [["k","清香"], ["l","甜"],["m","圓潤"],["n","濃厚"]]
+	#pacelist = [["o","家樂福"]]
+	#pricelist = [["p","351~600"],["q","100~350"],["r","600+"]]
 
-	drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"]\
-				,["afkop","冰靈麗絲玲黑皮諾微甜粉紅酒","黃尾袋鼠粉紅氣泡酒","邁坡莊園花漾微甜粉紅酒","南法小豬格納希粉紅酒"]]
-	addstr=''
+	#drinklist = [["aekop","冰靈麗絲玲黑皮諾微甜粉紅酒","義大利愛茉拉慕斯卡多甜白酒","南法小豬格納希粉紅酒","義大利若林慕斯卡微氣泡白酒","隆河紫羅蘭風土科倫坡白酒","德國丹赫1794慕斯卡白酒"]\
+	#			,["afkop","冰靈麗絲玲黑皮諾微甜粉紅酒","黃尾袋鼠粉紅氣泡酒","邁坡莊園花漾微甜粉紅酒","南法小豬格納希粉紅酒"]]
+	#addstr=''
+	#for y in range(0,2):
+		#if event.message.text == (drinklist[y][0]):
+			#for x in range(1,len(drinklist[y])):
+				#addstr += drinklist[y][x]+"\n"
+			#line_bot_api.reply_message(	
+        		#event.reply_token,
+        		#TextSendMessage(text="your choose->\n"+addstr))
 
-
-
-	for y in range(0,2):
-		if event.message.text == (drinklist[y][0]):
-			for x in range(1,len(drinklist[y])):
-				addstr += drinklist[y][x]+"\n"
-			line_bot_api.reply_message(	
-        		event.reply_token,
-        		TextSendMessage(text="your choose->\n"+addstr))
+    message = TemplateSendMessage(
+    alt_text='Buttons template',
+    template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='Menu',
+        text='Please select',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text'
+            ),
+            URITemplateAction(
+                label='uri',
+                uri='http://example.com/'
+            )
+        ]
+    )
+)
+line_bot_api.reply_message(event.reply_token, message)
 
 		
 
