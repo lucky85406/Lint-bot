ef06a0acabd6fabc6cf410d6c41e41544acd77f5
陳柏任 <lucky85406@gmail.com>
2019-11-05 00:09:06 +0800

    make it better

--- app.py
+++ app.py
@@ -50,38 +50,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
-	message = TemplateSendMessage(
-    alt_text='ImageCarousel template',
-    template=ImageCarouselTemplate(
-        columns=[
-            ImageCarouselColumn(
-                image_url='https://comps.canstockphoto.com.tw/abc-%E6%B6%B2%E4%BD%93-%E7%B3%BB%E5%88%97-%E6%95%B8%E5%AD%97-%E6%B0%B4-8-%E7%BE%8E%E5%B7%A5%E5%90%91%E9%87%8F_csp7832476.jpg',
-                action=PostbackTemplateAction(
-                    label='postback1',
-                    text='postback text1',
-                    data='action=buy&itemid=1'
-                )
-            ),
-            ImageCarouselColumn(
-                image_url='E:\line_chatbot\img\法旺紅酒.png',
-                action=PostbackTemplateAction(
-                    label='postback2',
-                    text='postback text2',
-                    data='action=buy&itemid=2'
-                )
-            ),
-            ImageCarouselColumn(
-                image_url='https://blognumbers.files.wordpress.com/2010/09/81.jpg',
-                action=PostbackTemplateAction(
-                    label='postback3',
-                    text='postback text3',
-                    data='action=buy&itemid=3'
-                ) 
-            )
-        ]
-    )
+	message = ImageSendMessage(
+    original_content_url='file:///E:/line_chatbot/img/%E6%B3%95%E6%97%BA%E7%B4%85%E9%85%92.png',
+    preview_image_url='file:///E:/line_chatbot/img/%E6%B3%95%E6%97%BA%E7%B4%85%E9%85%92.png'
 )
-	line_bot_api.reply_message(event.reply_token, message)
+line_bot_api.reply_message(event.reply_token, message)
 
 	#foodlist = [["a","白肉"], ["b","紅肉"],["c","海鮮"],["d","甜點"]]
 	#aromalist = [["e","花香"], ["f","漿果"],["g","柑橘"],["h","熱帶水果"],["i","淺色水果"],["j","香料"]]
