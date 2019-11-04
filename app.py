4bb04b3dc2472b55471b29a46ac899f6b34fd398
陳柏任 <lucky85406@gmail.com>
2019-11-04 20:54:43 +0800

    make it better

--- app.py
+++ app.py
@@ -13,7 +13,16 @@ from linebot.models import (
 )
 
 
-
+import pymssql  
+conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusername@yourserver', password='yourpassword', database='AdventureWorks')  
+cursor = conn.cursor()  
+cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")  
+row = cursor.fetchone()  
+while row:  
+        print "Inserted Product ID : " +str(row[0])  
+        row = cursor.fetchone()  
+conn.commit()
+conn.close()
 
 
 app = Flask(__name__)
