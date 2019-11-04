ba3664dfc057155c9aa595f2f95fe76b5108e3f9
陳柏任 <lucky85406@gmail.com>
2019-11-04 22:04:28 +0800

    make it better

--- app.py
+++ app.py
@@ -50,29 +50,29 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
-    message = TemplateSendMessage(
-    alt_text='Buttons template',
-    template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='Menu',
-        text='Please select',
-        actions=[
-            PostbackTemplateAction(
-                label='postback',
-                text='postback text',
-                data='action=buy&itemid=1'
+message = TemplateSendMessage(
+	alt_text='Buttons template',
+	template=ButtonsTemplate(
+		thumbnail_image_url='https://example.com/image.jpg',
+		title='Menu',
+		text='Please select',
+		actions=[
+		PostbackTemplateAction(
+			label='postback',
+			text='postback text',
+			data='action=buy&itemid=1'
             ),
-            MessageTemplateAction(
-                label='message',
-                text='message text'
+		MessageTemplateAction(
+			label='message',
+			text='message text'
             ),
-            URITemplateAction(
-                label='uri',
-                uri='http://example.com/'
-            )
-        ]
-    )
-)
+		URITemplateAction(
+			label='uri',
+			uri='http://example.com/'
+			)
+		]
+		)
+	)
 line_bot_api.reply_message(event.reply_token, message)
 
 		
