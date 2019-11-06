7baf98785d7f337561ad869bbdf1eb6f8916ad2e
陳柏任 <lucky85406@gmail.com>
2019-11-07 04:15:23 +0800

    make it better

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def function(event):
 	addstr=''
 	urlstr=''
 	fistr=''
-	rx = []*len(drinklist)
+	rx = [1]*len(drinklist)
 	for y in range(0,len(drinklist)):
 		if event.message.text == (drinklist[y][0]):
 			print(len(rx))
