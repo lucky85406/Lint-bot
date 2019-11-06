81e937785f1ccfc24c1e914e107e33ab7f7f0924
陳柏任 <lucky85406@gmail.com>
2019-11-07 03:38:42 +0800

    make it better

--- app.py
+++ app.py
@@ -92,13 +92,13 @@ def function(event):
 	)
 
 	for tx in range(0,9):
-		rx = random_int_list(1,10,1)
+		rx[tx] = random_int_list(1,10,1)
 
 	for y in range(0,len(drinklist)):
 		if event.message.text == (drinklist[y][0]):
 			for x in range(1,len(drinklist[y])):	
 				addstr += drinklist[y][x]+"\n"
-				print(rx[x])
+				print(rx[x-1])
 			line_bot_api.reply_message(	
         		event.reply_token,
         		TextSendMessage(text= chr(0x100079)+"your choose->\n"+addstr+"\n"+chr(0x100091)))
