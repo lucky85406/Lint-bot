d73b6aa70a7fa507b08feb011ee2f85ad6144309
陳柏任 <lucky85406@gmail.com>
2019-11-07 02:30:30 +0800

    make it better

--- app.py
+++ app.py
@@ -47,13 +47,7 @@ def callback():
 
     return 'OK'
 
-def random_int_list(start, stop, length):
-	start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
-	length = int(abs(length)) if length else 0
-	random_list = []
-	for i in range(length):
-	random_list.append(random.randint(start, stop))
-	return random_list
+
 
 
 # 處理訊息
