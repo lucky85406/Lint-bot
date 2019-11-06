24498ddeda7ca9f08287855e6e2ee0076ccd54fc
陳柏任 <lucky85406@gmail.com>
2019-11-07 03:00:34 +0800

    make it better

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def function(event):
 	addstr=''
 	urlstr=''
 	fistr=''
-	rx[]=random_int_list(1,4,1)
+	rx=random_int_list(1,4,1)
 	print(rx[0])
 	for cl in range(0,len(urllistc)):	
 		if event.message.text == urllistc[cl][0]:
