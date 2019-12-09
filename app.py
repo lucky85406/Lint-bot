c4a156c90ddccd15923642c83097c99087932b7a
陳柏任 <lucky85406@gmail.com>
2019-12-10 01:27:45 +0800

    make it better

--- app.py
+++ app.py
@@ -306,9 +306,9 @@ def function(event):
 
 #測試輸出文件
 	if event.message.text == "測試":
-		with open('test.txt','w')as f:
-			f.write('hello\n')
-			f.write('world')
+		with open('test.txt','r')as f:
+			data = f.read()
+			print(data)
 
 
 		
