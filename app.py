8f71886efd0aa4bc8d921f39b9ab9177cfdd5a47
陳柏任 <lucky85406@gmail.com>
2019-12-10 02:47:57 +0800

    make it better

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
 def function(event):
 	#測試輸出文件
 	f = open(r'E:/line_chatbot/test_io.txt','w')
-	f.write("123")
+	f.write("321")
 	f.close()
 	# 資料源
 	drinklist = [["拉圖城堡紅酒","https://i.imgur.com/diorIgW.jpg","afnsv","ajnsv","bfnsv","bjnsv"],
