1752733c1a305359c88f3a6139c6389747089738
陳柏任 <lucky85406@gmail.com>
2019-12-16 15:05:54 +0800

    ok

--- dataList.py
+++ dataList.py
@@ -4,7 +4,7 @@ Lcode = ['L1','L2','L3','L4','L5','L6','L7','L8','L9','L10','L11','L12','L13','L
 a=0
 set1 = set()
 def dataList(intxt):
-	with open('cosurl.csv',newline='')as csvfile:
+	with open('cosurl.csv',newline='',encoding='gbk')as csvfile:
 		rows = csv.DictReader(csvfile)
 		for row in rows:
 			for x in range(0,18):
