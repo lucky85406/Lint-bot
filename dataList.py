4cc2c959a6ade510853c9d73b9d0acab71ff0e46
陳柏任 <lucky85406@gmail.com>
2019-12-16 15:41:51 +0800

    ok

--- dataList.py
+++ dataList.py
@@ -1,6 +1,7 @@
 import csv
 import random
 Lcode = ['L1','L2','L3','L4','L5','L6','L7','L8','L9','L10','L11','L12','L13','L14','L15','L16','L17','L18']
+pngurl="https://stickersofast.com/images/transparent_500.jpg?3"
 def dataList(intxt):
 	set1 = set()
 	set2 = set()
@@ -11,5 +12,14 @@ def dataList(intxt):
 			for x in range(0,18):
 				if row[Lcode[x]] == s:
 					set1.add(row['url'])
-	set2 = random.sample(set1,10)
+	a = len(set1)
+	if a >=10:
+		set2 = random.sample(set1,10)
+	else:
+		li = list(set2)
+		for x in range(0,10):
+			if li[x] == "":
+				li[x] = pngurl
+		set2 = set(li)		
+
 	return set2
