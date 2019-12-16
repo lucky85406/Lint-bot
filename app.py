b19fe2cbfe0cb5cca0e6b8e3d3c1b60c315beb6d
陳柏任 <lucky85406@gmail.com>
2019-12-16 16:34:21 +0800

    ok

--- app.py
+++ app.py
@@ -9,34 +9,15 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction, ImageCarouselColumn, ImageCarouselTemplate, ImageSendMessage, URIImagemapAction, MessageImagemapAction
     )
-from oauth2client.service_account import ServiceAccountCredentials 
+
 
 from dataList import (dataList,dataOpen)
 import random
-import gspread
 import time #待會會取時間
 
 
 
-auth_json_path = 'PythonUpload-841c8b986f44.json' #由剛剛建立出的憑證，放置相同目錄以供引入
-gss_scopes = ['https://spreadsheets.google.com/feeds'] #我們想要取用的範圍
 
-
-
-def auth_gss_client(path, scopes):
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
-    return gspread.authorize(credentials)
-
-gss_client = auth_gss_client(auth_json_path, gss_scopes) #呼叫我們的函式
-
-
-#從剛剛建立的sheet，把網址中 https://docs.google.com/spreadsheets/d/〔key〕/edit 的 〔key〕的值代入 
-spreadsheet_key_path = '1vhiAa6idyIwIkVVZdTXzhAHKclf9lvb5j4PXhsodWXM'
-
-#我們透過open_by_key這個method來開啟sheet
-sheet = gss_client.open_by_key(spreadsheet_key_path).sheet1
-#單純取出時間稍後塞入sheet
-today = time.strftime("%c")
 
 
 app = Flask(__name__)
@@ -85,8 +66,7 @@ def function(event):
 	dlist = list(data)
 
 	print(dataOpen(judstr))
-	#透過insert_row寫入值 第二行塞入時間,abc,123的值
-	sheet.insert_row([today,judstr, 123], 2)
+
 
 	if dlen<10:
 		for x in range(dlen,10):
