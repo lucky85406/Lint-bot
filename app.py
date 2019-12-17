6695f29a5d4e808e0e7c9add5322e8e47511303b
陳柏任 <lucky85406@gmail.com>
2019-12-17 11:06:54 +0800

    ok

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def function(event):
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
 	with open("usertxt.txt","w") as f:
-		mes = f.write("第{}:".format(a)+mes+event.message.text+"\n")
-		a=a+1
+		mes = f.write(mes+"\n"+event.message.text)
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
 		if event.message.text == "show":
