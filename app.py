1181003623c203566cd733bd5992f938ec757885
陳柏任 <lucky85406@gmail.com>
2019-12-17 11:00:18 +0800

    ok

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def function(event):
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
 	with open("usertxt.txt","w") as f:
-		mes = f.write(a+mes+event.message.text+"\n")
+		mes = f.write("第{}:{}".format(a,mes+event.message.text+"\n"))
 		a=a+1
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
