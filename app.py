6d242a07a949c88d0650b07461475ccbfe027b06
陳柏任 <lucky85406@gmail.com>
2019-12-17 11:12:57 +0800

    ok

--- app.py
+++ app.py
@@ -48,12 +48,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
 	mes = ""
-	li="list"
+	li="list: "
 	a=0
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
 	with open("usertxt.txt","w") as f:
-		mes = f.write(li+event.message.text+"\n")
+		mes = f.write(li+event.message.text+mes+"\n")
 	with open("usertxt.txt","r") as f:
 		mes = f.read()
 		line_bot_api.reply_message(	
