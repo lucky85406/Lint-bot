e6d6d46a7eae68324ad2707ea15cc615b04e78ba
陳柏任 <lucky85406@gmail.com>
2019-12-19 13:23:53 +0800

    ok

--- utoken.py
+++ utoken.py
@@ -5,9 +5,12 @@ def outU():
 	lis = list()
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
+		lis = rows
 		for row in rows:
 			print(row['Utoken'],row['U1'],row['U2'],row['U3'],row['U4'],row['U5'])
 	return lis
+
+		
 		
 def  inID(uid):
 	with open('user.csv', 'w' ,newline='') as csvf:
