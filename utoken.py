754219dfb162f74a60e4dd1467fdb31102cfffb6
陳柏任 <lucky85406@gmail.com>
2019-12-19 12:08:04 +0800

    ok

--- utoken.py
+++ utoken.py
@@ -4,8 +4,7 @@ import random
 def outU():
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
-		for row in rows:
-			print(row['Utoken'],row['U1'],row['U2'],row['U3'],row['U4'],row['U5'])
+		print(rows)
 def  inID(uid):
 	with open('user.csv', 'w' ,newline='') as csvf:
 
