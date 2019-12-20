8c8f59c88db23c6b91621c97a3c27045ba24bfdc
陳柏任 <lucky85406@gmail.com>
2019-12-20 12:54:26 +0800

    ok

--- utoken.py
+++ utoken.py
@@ -5,7 +5,6 @@ def outU(uid):
 	
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
-		lis = rows
 		for row in rows:
 			if uid == row['Utoken']:
 				return "ID:\n{}\n1: {}\n2: {}\n3: {}\n4: {}\n5: {}".format(row['Utoken'],row['U1'],row['U2'],row['U3'],row['U4'],row['U5'])
@@ -14,18 +13,22 @@ def outU(uid):
 		
 		
 def  inID(uid):
+	x=0
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
 		for row in rows:
 			if row['Utoken'] != uid:
-				with open('user.csv', 'w' ,newline='') as csvf:
+				x = x+1
+		with open('user.csv', 'w' ,newline='') as csvf:
+			filedn = ['Utoken','U1','U2','U3','U4','U5']
 
-					filedn = ['Utoken','U1','U2','U3','U4','U5']
+			writer = csv.DictWriter(csvf,fieldnames=filedn)
 
-					writer = csv.DictWriter(csvf,fieldnames=filedn)
-
-					writer.writeheader()
-					writer.writerow({'Utoken':uid,'U1':"a",'U2':"e",'U3':"l",'U4':"q",'U5':"t"})
+			writer.writeheader()
+			for row in rows:
+				for y in range(0,x):
+					if row['Utoken'] == uid:
+						writer.writerow({'Utoken':uid,'U1':"a",'U2':"e",'U3':"l",'U4':"q",'U5':"t"})
 def ina(uid,x):
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
