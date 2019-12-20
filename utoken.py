652b960eecf2697d7348d3d80274d0e43964a969
陳柏任 <lucky85406@gmail.com>
2019-12-20 13:09:07 +0800

    ok

--- utoken.py
+++ utoken.py
@@ -16,8 +16,8 @@ def  inID(uid):
 	with open('user.csv', 'w' ,newline='') as csvf:
 		filedn = ['Utoken','U1','U2','U3','U4','U5']
 		writer = csv.DictWriter(csvf,fieldnames=filedn)
-		writer.writeheader()
-		writer.writerows({'Utoken':uid,'U1':"a",'U2':"e",'U3':"l",'U4':"q",'U5':"t"})
+		writer.writerows({'Utoken','U1','U2','U3','U4','U5'})
+		writer.writerows({uid,"a","e","l","q","t"})
 def ina(uid,x):
 	with open('user.csv',newline='') as csvf:
 		rows = csv.DictReader(csvf)
