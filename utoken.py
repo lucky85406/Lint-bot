b7ff9aedd55b867cec6d97ad6ccce673b42b73b3
陳柏任 <lucky85406@gmail.com>
2019-12-21 16:28:39 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -3,14 +3,14 @@ import random
 import json
 
 def go(url,x,n):
-with open('user.csv','a+',newline='')as cfile:
-		fieldn = ['Utoken','U1','n']
+	with open('user.csv','a+',newline='')as cfile:
+			fieldn = ['Utoken','U1','n']
 
-		writer = csv.DictWriter(cfile,fieldnames=fieldn)
+			writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
-		writer.writeheader()
+			writer.writeheader()
 
-		writer.writerow({'Utoken':url,'U1':x,'n':n})
+			writer.writerow({'Utoken':url,'U1':x,'n':n})
 
 def inMes(url,x):
 	with op('user.csv',newline) as c:
