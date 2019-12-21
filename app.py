c9930683f7226d7c1af12f6a14ad913c6edfcfa1
陳柏任 <lucky85406@gmail.com>
2019-12-21 16:47:46 +0800

    OK

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def function(event):
 				TextSendMessage(text= showMes(user_id)))
 	elif ukey =="go":
 		user_id = event.source.user_id
-		go(user_id,"b",1)
+		go(user_id,"b","1")
 	else:
 		user_id = event.source.user_id
 		inMes(user_id,ukey)
