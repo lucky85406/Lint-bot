707e2bdeb30299cb81447205f6b6dd08fce3a52e
陳柏任 <lucky85406@gmail.com>
2019-12-21 19:26:10 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -21,48 +21,53 @@ def inMes(url,x):
 			b = row['Ver']
 			if row['ID'] == url:
 				if  x == "a" or x == "b" or x == "c" or x == "d":
-					with open('user.csv','a',newline='')as cfile:
-							fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+					if row['Ver']=="1":
+						with open('user.csv','a',newline='')as cfile:
+								fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
-							writer = csv.DictWriter(cfile,fieldnames=fieldn)
+								writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
-							writer.writerow({'ID':url,'D1':x,'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
+								writer.writerow({'ID':url,'D1':x,'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
 
 				elif x == "e" or x == "f" or x == "g" or x == "h" or x == "i" or x =="j" or x =="k":
-					with open('user.csv','a',newline='')as cfile:
-							fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+					if row['Ver']=="2":
+						with open('user.csv','a',newline='')as cfile:
+								fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
-							writer = csv.DictWriter(cfile,fieldnames=fieldn)
+								writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 
-							writer.writerow({'ID':url,'D1':row['D1'],'D2':x,'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
+								writer.writerow({'ID':url,'D1':row['D1'],'D2':x,'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
 
 				elif x == "l" or x == "m" or x == "n" or x == "o" or x == "p":
-					with open('user.csv','a',newline='')as cfile:
-							fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+					if row['Ver']=="3":
+						with open('user.csv','a',newline='')as cfile:
+								fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
-							writer = csv.DictWriter(cfile,fieldnames=fieldn)
+								writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 
-							writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':x,'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
+								writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':x,'D4':row['D4'],'D5':row['D5'],'Ver':int(b)+1})
 
 				elif x == "q" or x == "r" or x == "s":
-					with open('user.csv','a',newline='')as cfile:
-							fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+					if row['Ver']=="4":
+						with open('user.csv','a',newline='')as cfile:
+								fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
-							writer = csv.DictWriter(cfile,fieldnames=fieldn)
+								writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 
-							writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':x,'D5':row['D5'],'Ver':int(b)+1})
+								writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':x,'D5':row['D5'],'Ver':int(b)+1})
 
 				elif x == "t" or x == "u" or x == "v":
-					with open('user.csv','a',newline='')as cfile:
-							fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+					if row['Ver']=="5":
+						with open('user.csv','a',newline='')as cfile:
+								fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
-							writer = csv.DictWriter(cfile,fieldnames=fieldn)
+								writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 
-							writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':x,'Ver':int(b)+1})
+								writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':x,'Ver':int(b)+1})
 
 	
 
