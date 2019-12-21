c08226d367d652e3c3a591bb43798ea3a3e05837
陳柏任 <lucky85406@gmail.com>
2019-12-21 21:18:32 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -13,37 +13,53 @@ def go(url):
 			writer.writerow({'ID':url,'D1':"",'D2':"",'D3':"",'D4':"",'D5':"",'Ver':"0"})
 
 def inMes(url,x):
+	Dlist = [""]*5
 	ver = ""
-
-	if x == "a" or x =="b" or x =="c" or x =="d":
-		ver = "1"
-	elif x == "e" or x =="f" or x =="g" or x =="h" or x =="i" or x =="j" or x =="k":
-		ver ="2"
-	elif x == "l" or x =="m" or x =="n" or x =="o" or x =="p":
-		ver ="3"
-	elif x == "q" or x =="r" or x =="s":
-		ver ="4"
-	elif x == "y" or x =="u" or x =="v":
-		ver ="5"
-
-	with open('user.csv',newline='')as cfile:
-		rows = csv.DictReader(cfile)
+	with open('user.csv',newline='') as csvfile:
+		rows = csv.DictReader(csvfile)
 		for row in rows:
-			with open('user.csv','a',newline='')as cfile:
-					fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+			if row['ID'] == url and row['Ver'] == "0" and row['D1'] == "":
+				Dlist[0] = x
+				Dlist[1] = row['D2']
+				Dlist[2] = row['D3']
+				Dlist[3] = row['D4']
+				Dlist[4] = row['D5']
+				ver = "1"
+			elif row['ID'] == url and row['Ver'] == "1" and row['D2'] == "":
+				Dlist[0] = row['D1']
+				Dlist[1] = x
+				Dlist[2] = row['D3']
+				Dlist[3] = row['D4']
+				Dlist[4] = row['D5']
+				ver = "2"
+			elif row['ID'] == url and row['Ver'] == "2" and row['D3'] == "":
+				Dlist[0] = row['D1']
+				Dlist[1] = row['D2']
+				Dlist[2] = x
+				Dlist[3] = row['D4']
+				Dlist[4] = row['D5']
+				ver = "3"
+			elif row['ID'] == url and row['Ver'] == "3" and row['D4'] == "":
+				Dlist[0] = row['D1']
+				Dlist[1] = row['D2']
+				Dlist[2] = row['D3']
+				Dlist[3] = x
+				Dlist[4] = row['D5']
+				ver = "4"
+			elif row['ID'] == url and row['Ver'] == "4" and row['D5'] == "":
+				Dlist[0] = row['D1']
+				Dlist[1] = row['D2']
+				Dlist[2] = row['D3']
+				Dlist[3] = row['D4']
+				Dlist[4] = x
+				ver = "5"			
 
-					writer = csv.DictWriter(cfile,fieldnames=fieldn)
-					if row['ID'] == url and row['Ver'] == '0':
-						writer.writerow({'ID':url,'D1':x,'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':ver})
-					elif row['ID'] == url and row['Ver'] == '1':
-						writer.writerow({'ID':url,'D1':row['D1'],'D2':x,'D3':row['D3'],'D4':row['D4'],'D5':row['D5'],'Ver':ver})
-					elif row['ID'] == url and row['Ver'] == '2':
-						writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':x,'D4':row['D4'],'D5':row['D5'],'Ver':ver})
-					elif row['ID'] == url and row['Ver'] == '3':
-						writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':x,'D5':row['D5'],'Ver':ver})
-					elif row['ID'] == url and row['Ver'] == '4':
-						writer.writerow({'ID':url,'D1':row['D1'],'D2':row['D2'],'D3':row['D3'],'D4':row['D4'],'D5':x,'Ver':ver})					
-			
+	with open('user.csv','a',newline='')as cfile:
+			fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
+
+			writer = csv.DictWriter(cfile,fieldnames=fieldn)
+
+			writer.writerow({'ID':url,'D1':Dlist[0],'D2':Dlist[1],'D3':Dlist[2],'D4':Dlist[3],'D5':Dlist[4],'Ver':ver})			
 	
 
 def showMes(url):
