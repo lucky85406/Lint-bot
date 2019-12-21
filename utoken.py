36625bea5d65dec019b1bc3a545740373cb5ea60
陳柏任 <lucky85406@gmail.com>
2019-12-21 16:04:32 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -20,15 +20,16 @@ def inMes(url,x):
 
 		writer.writeheader()
 
-		writer.writerow({'Utoken':url,'U1':"x",'U2':"x",'U3':x,'U4':"x",'U5':"x"})
+		writer.writerow({'Utoken':url,'U1':"2",'U2':"x",'U3':x,'U4':"x",'U5':"x"})
 def showMes(url):
 	with open('user.csv',newline='') as csvfile:
 		rows = csv.DictReader(csvfile)
 		a =0
 		for row in rows:
 			b =str(a)
-			if row['Utoken'] == url and row['U1'] == '2':
+			if row['Utoken'] == url and row['U1'] == b:
 				return "ID:\n{}\nU1: {}\nU2: {}\nU3: {}\nU4: {}\nU5: {}\n".format(row['Utoken'],row['U1'],row['U2'],row['U3'],row['U4'],row['U5'])
 			else:
-				return "1"
 				a = a+1	
+				return "1"
+				
