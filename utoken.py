47607cad33caaf3f9ed08823e32fc91b30d86960
陳柏任 <lucky85406@gmail.com>
2019-12-21 17:49:05 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -13,13 +13,22 @@ def go(url,x,n):
 			writer.writerow({'Utoken':url,'U1':x,'n':n})
 
 def inMes(url,x):
+	a=0
+	b=""
+	with open('user.csv',newline='') as csvfile:
+		rows = csv.DictReader(csvfile)
+		for row in rows:
+			b = row['n']
+			if int(b)>0:
+				a = a+1
+
 	with open('user.csv','a',newline='')as cfile:
 			fieldn = ['Utoken','U1','n']
 
 			writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 
-			writer.writerow({'Utoken':url,'U1':x,'n':"2"})
+			writer.writerow({'Utoken':url,'U1':x,'n':a})
 
 	
 def showMes(url):
