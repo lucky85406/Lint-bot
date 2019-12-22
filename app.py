f9c89de8f04952970f42ae32a2863f8dd4a21b6d
陳柏任 <lucky85406@gmail.com>
2019-12-22 14:28:20 +0800

    OK

--- app.py
+++ app.py
@@ -67,6 +67,31 @@ def function(event):
 	elif ukey =="go":
 		user_id = event.source.user_id
 		go(user_id)
+	elif ukey =="menu":
+		message = TemplateSendMessage(
+			alt_text='Buttons template',
+			template=ButtonsTemplate(
+				thumbnail_image_url='https://example.com/image.jpg',
+				title='Menu',
+				text='Please select',
+				actions=[
+					PostbackTemplateAction(
+						label='postback',
+						text='postback text',
+						data='action=buy&itemid=1'
+					),
+					MessageTemplateAction(
+						label='message',
+						text='message text'
+					),
+					URITemplateAction(
+						label='uri',
+						uri='http://example.com/'
+					)
+				]
+			)
+		)
+		line_bot_api.reply_message(event.reply_token, message)
 	else:
 		user_id = event.source.user_id
 		inMes(user_id,ukey)
