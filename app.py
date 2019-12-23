144d7f398bfbab41b4c32aee0cdf230d8bd7f890
陳柏任 <lucky85406@gmail.com>
2019-12-23 20:11:07 +0800

    OK

--- app.py
+++ app.py
@@ -286,79 +286,162 @@ def function(event):
 		inMes(user_id,ukey)
 '''
 	if event.message.text == "我要測試":
-		ISM = BubbleContainer(
-			header = BoxComponent(
-				layout = 'baseline',
-				contents = [
-					TextComponent(
-						text = '示範',
-						weight = 'bold',
-						size = 'xxl',
-						flex =2,
-						align = 'center'
+		ISM = CarouselTemplate(
+				columns = [
+					CarouselColumn(
+						BubbleContainer(
+							header = BoxComponent(
+								layout = 'baseline',
+								contents = [
+									TextComponent(
+										text = '示範',
+										weight = 'bold',
+										size = 'xxl',
+										flex =2,
+										align = 'center'
+									)
+								]
+							),
+							body = BoxComponent(
+								layout = 'vertical',
+								spacing = 'xs',
+								contents = [
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									)										
+								]
+							),
+							footer = BoxComponent(
+								layout = 'baseline',
+								contents = [
+									TextComponent(
+										text = '示範',
+										weight = 'bold',
+										size = 'xxl',
+										flex =2,
+										align = 'center'
+									)
+								]
+							)		
 						)
-					]
-				),
-			body = BoxComponent(
-				layout = 'vertical',
-				spacing = 'xs',
-				contents = [
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
+
 					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					),
-					ButtonComponent(
-						style='secondary',
-						color='#FFEE99',
-						height='sm',
-						action=MessageAction(label='設定起始數字',text='測試')
-					)										
-					]
-				),
-			footer = BoxComponent(
-				layout = 'baseline',
-				contents = [
-					TextComponent(
-						text = '示範',
-						weight = 'bold',
-						size = 'xxl',
-						flex =2,
-						align = 'center'
+					CarouselColumn(
+						BubbleContainer(
+							header = BoxComponent(
+								layout = 'baseline',
+								contents = [
+									TextComponent(
+										text = '示範',
+										weight = 'bold',
+										size = 'xxl',
+										flex =2,
+										align = 'center'
+									)
+								]
+							),
+							body = BoxComponent(
+								layout = 'vertical',
+								spacing = 'xs',
+								contents = [
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									),
+									ButtonComponent(
+										style='secondary',
+										color='#FFEE99',
+										height='sm',
+										action=MessageAction(label='設定起始數字',text='測試')
+									)										
+								]
+							),
+							footer = BoxComponent(
+								layout = 'baseline',
+								contents = [
+									TextComponent(
+										text = '示範',
+										weight = 'bold',
+										size = 'xxl',
+										flex =2,
+										align = 'center'
+									)
+								]
+							)		
 						)
-					]
-				)		
+					)
+				]
 			)
 
 			
