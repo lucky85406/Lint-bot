f3e3877955d80aa544b2a77e257d8e6a0485df51
陳柏任 <lucky85406@gmail.com>
2019-12-26 23:52:07 +0800

    OK

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def callback():
 
     return 'OK'
 
-
+# 食物清單
 def food():
 	mes = BubbleContainer(
 			header = BoxComponent(
@@ -102,6 +102,228 @@ def food():
 	message = FlexSendMessage(alt_text = "hello",contents = mes)
 	return message
 
+# 香氣清單
+def aroma():
+	mes = BubbleContainer(
+			header = BoxComponent(
+				layout = 'baseline',
+				contents = [
+					TextComponent(
+						text = 'Q2:喜歡的香氣',
+						weight = 'bold',
+						size = 'xxl',
+						flex =2,
+						align = 'center'
+					)
+				]
+			),
+			body = BoxComponent(
+				layout = 'vertical',
+				spacing = 'xs',
+				contents = [
+					ButtonComponent(
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='花香',text='花香')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='漿果',text='漿果')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='柑橘',text='柑橘')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='熱帶水果',text='熱帶水果')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='淺色水果',text='淺色水果')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='香料',text='香料')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='土木',text='土木')
+					)																	
+				]
+			)		
+		)
+	message = FlexSendMessage(alt_text = "hello",contents = mes)
+	return message
+
+# 口感清單
+def taste():
+	mes = BubbleContainer(
+			header = BoxComponent(
+				layout = 'baseline',
+				contents = [
+					TextComponent(
+						text = 'Q3:想要的口感',
+						weight = 'bold',
+						size = 'xxl',
+						flex =2,
+						align = 'center'
+					)
+				]
+			),
+			body = BoxComponent(
+				layout = 'vertical',
+				spacing = 'xs',
+				contents = [
+					ButtonComponent(
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='清香',text='清香')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='甜',text='甜')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='圓潤',text='圓潤')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='酸',text='酸')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='濃厚',text='濃厚')
+					)																	
+				]
+			)		
+		)
+	message = FlexSendMessage(alt_text = "hello",contents = mes)
+	return message
+
+#通路
+def chain():
+	mes = BubbleContainer(
+			header = BoxComponent(
+				layout = 'baseline',
+				contents = [
+					TextComponent(
+						text = 'Q4:想購買的通路',
+						weight = 'bold',
+						size = 'xxl',
+						flex =2,
+						align = 'center'
+					)
+				]
+			),
+			body = BoxComponent(
+				layout = 'vertical',
+				spacing = 'xs',
+				contents = [
+					ButtonComponent(
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='家樂福',text='家樂福')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='全聯',text='全聯')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='Costco',text='Costco')
+					)																	
+				]
+			)		
+		)
+	message = FlexSendMessage(alt_text = "hello",contents = mes)
+	return message
+
+#價格清單
+def price():
+	mes = BubbleContainer(
+			header = BoxComponent(
+				layout = 'baseline',
+				contents = [
+					TextComponent(
+						text = 'Q5:價格預算',
+						weight = 'bold',
+						size = 'xxl',
+						flex =2,
+						align = 'center'
+					)
+				]
+			),
+			body = BoxComponent(
+				layout = 'vertical',
+				spacing = 'xs',
+				contents = [
+					ButtonComponent(
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='100~350元',text='100~350元')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='351~600元',text='351~600元')
+					),
+					ButtonComponent(
+						margin = 'xl',
+						style='secondary',
+						color='#FFEE99',
+						height='sm',
+						action=MessageAction(label='600元以上',text='600元以上')
+					)																	
+				]
+			)		
+		)
+	message = FlexSendMessage(alt_text = "hello",contents = mes)
+	return message
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def function(event):
@@ -128,8 +350,16 @@ def function(event):
 		user_id = event.source.user_id
 		inMes(user_id,ukey)
 '''
-	if event.message.text == "我要測試":
+	if event.message.text == "食物清單":
 		line_bot_api.reply_message(event.reply_token,food())
+	elif event.message.text == "香氣清單":
+		line_bot_api.reply_message(event.reply_token,aroma())
+	elif event.message.text == "口感清單":
+		line_bot_api.reply_message(event.reply_token,taste())
+	elif event.message.text == "通路清單":
+		line_bot_api.reply_message(event.reply_token,chain())
+	elif event.message.text == "價格清單":
+		line_bot_api.reply_message(event.reply_token,price())
 
 
 # 執行
