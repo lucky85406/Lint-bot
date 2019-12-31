3b724e435450d6edf684d18349b598d837f046dc
陳柏任 <lucky85406@gmail.com>
2019-12-31 13:47:36 +0800

    OK

--- app.py
+++ app.py
@@ -374,6 +374,13 @@ def testimg():
 						color = '#0000FF'
 					),
 					TextComponent(
+						text = '敘述:',
+						weight = 'bold',
+						size = 'xs',	
+						align = 'start',
+						color = '#FF00FF'
+					),
+					TextComponent(
 						text = '敘述:故事敘述法，就是像講故事一樣,',
 						weight = 'bold',
 						size = 'xs',	
@@ -381,14 +388,21 @@ def testimg():
 						color = '#FF00FF'
 					),
 					TextComponent(
-						text = '	把事情的前因後果、來龍去脈、出場人物、',
+						text = '把事情的前因後果、來龍去脈、出場人物、',
 						weight = 'bold',
 						size = 'xs',	
 						align = 'start',
 						color = '#FF00FF'
 					),
 					TextComponent(
-						text = '	發展過程、結局和解決方案都說明白的一種方法。',
+						text = '發展過程、結局和解決方案',
+						weight = 'bold',
+						size = 'xs',	
+						align = 'start',
+						color = '#FF00FF'
+					),
+					TextComponent(
+						text = '都說明白的一種方法。',
 						weight = 'bold',
 						size = 'xs',	
 						align = 'start',
