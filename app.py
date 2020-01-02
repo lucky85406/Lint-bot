66aae4836abf923a6e314bd6f878f921fbc80d12
陳柏任 <lucky85406@gmail.com>
2020-01-02 13:54:15 +0800

    OK

--- app.py
+++ app.py
@@ -529,7 +529,7 @@ def function(event):
 			line_bot_api.reply_message(event.reply_token,Image_Carousel)
 		elif ukey =="testimg":
 			user_id = event.source.user_id
-			b = event.source.displayName
+			b = event.UserInfo.displayName
 			a = test(user_id,b)
 			line_bot_api.reply_message(event.reply_token,testimg(a))
 
