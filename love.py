7fa41c00d9dce004d6231f7f43ad9fb503821e6e
陳柏任 <lucky85406@gmail.com>
2020-01-03 17:18:35 +0800

    OK

--- love.py
+++ love.py
@@ -11,5 +11,5 @@ def love(uid,text):
 	with open('mylove.csv',newline='')as csvfile:
 		rows = csv.DictReader(csvfile)
 		for row in rows:
-			if row['ID'] == "U7a3fa520f67563a2429f22e86b798981":
+			if row['ID'] == "柏任":
 				print(row['ID']+":"+row['love'])	
