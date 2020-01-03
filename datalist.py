868a82c5f62bb6961920259df4b817f6c67059cf
陳柏任 <lucky85406@gmail.com>
2020-01-03 14:31:06 +0800

    OK

--- datalist.py
+++ datalist.py
@@ -3,15 +3,15 @@ import random
 
 Lcode = ['L1','L2','L3','L4','L5','L6','L7','L8','L9','L10','L11','L12','L13','L14','L15','L16','L17','L18']
 def dataList(intxt):
-	set1 = set()
-	set2 = set()
+	set1 = {}
+	set2 = {}
 	s = intxt
 	with open('cosurl.csv',newline='')as csvfile:
 		rows = csv.DictReader(csvfile)
 		for row in rows:
 			for x in range(0,18):
 				if row[Lcode[x]] == s:
-					set1.add(row['url'])
+					set1[row['url']] = row['酒類']
 	a = len(set1)
 	if a >=10:
 		set2 = random.sample(set1,10)
