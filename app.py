1df71d95d853ce572378d879c943c29b798bc500
陳柏任 <lucky85406@gmail.com>
2020-01-03 17:16:24 +0800

    OK

--- app.py
+++ app.py
@@ -550,9 +550,10 @@ def function(event):
 			user_id = event.source.user_id
 			line_bot_api.reply_message(event.reply_token,testimg())
 		elif ukey.split(':')[0] == 'MyLove':
-			print("OK"+ukey)
 			user_id = event.source.user_id
-			love(user_id,ukey)
+			profile = line_bot_api.get_profile(user_id)
+			name = profile.display_name
+			love(name,ukey)
 
 
 
