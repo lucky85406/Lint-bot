f780a5a3655eb1781adfcf8bdfe0a3c5049e8e81
陳柏任 <lucky85406@gmail.com>
2020-01-03 18:20:02 +0800

    OK

--- imgmessage.py
+++ imgmessage.py
@@ -5,7 +5,7 @@ from linebot.models import (
 )
 
 def test():
-	a = {BubbleContainer(
+	a = [BubbleContainer(
 				hero = ImageComponent(
 							url = 'https://i.imgur.com/diorIgW.jpg',
 							size = 'full',
@@ -58,8 +58,8 @@ def test():
 								)
 							]
 				)			
-			)}
-	return list(a)[0]
+			)]
+	return a[0]
 
 #10
 def tenMod(img,dset):
