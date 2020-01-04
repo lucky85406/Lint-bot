c2ad5fe8e7f02b202a3b8d3b459e5da62cde8820
陳柏任 <lucky85406@gmail.com>
2020-01-04 12:10:40 +0800

    OK

--- modelCode.py
+++ modelCode.py
@@ -321,7 +321,7 @@ def mylove(n,ulist):
 						contents = [
 							ButtonComponent(
 								style='link',
-								color='#ACD6FF',
+								color='#84C1FF',
 								flex = 3,
 								height='sm',
 								action=MessageAction(label=a[0],text='showImg')
@@ -391,4 +391,71 @@ def mylove(n,ulist):
 	)
 
 	message = FlexSendMessage(alt_text = "hello",contents = Carousel)
+	return message
+
+#10
+def tenMod(img,dset):
+	d = dset
+	imglist = img
+	contents = []
+	dlen = len(imglist)
+	for x in range(0,dlen):
+		contents.append(BubbleContainer(
+				hero = ImageComponent(
+							url = imglist[x],
+							size = 'full',
+							align = 'center',
+							aspect_ratio = '13:13',
+							aspect_mode = 'cover'
+				),
+				body = BoxComponent(
+							layout = 'vertical',
+							contents = [
+								TextComponent(
+									margin = 'md',
+									text = d[imglist[x]],
+									weight = 'bold',
+									size = 'md',
+									align = 'center'
+								),
+								TextComponent(
+									margin = 'md',
+									text = "第一段",
+									weight = 'bold',
+									size = 'xs',
+									align = 'center'
+								),
+								SeparatorComponent(
+									margin = 'xl',
+									color = '#0000FF'
+								),
+								BoxComponent(
+									margin = 'md',
+									layout = 'horizontal',
+									spacing = 'xs',
+									contents = [
+										ButtonComponent(
+											style='link',
+											color='#84C1FF',
+											flex = 1,
+											height='sm',
+											action=MessageAction(label="加入最愛",text='MyLove:'+d[imglist[x]])
+										),
+										ButtonComponent(
+											margin = 'xxl',
+											style='link',
+											color='#84C1FF',
+											flex = 1,
+											height='sm',
+											action=MessageAction(label="還沒想到",text='還沒想到')
+										)
+									]
+								)
+							]
+				)			
+			))
+	Carousel = CarouselContainer(
+		contents = contents		
+	)
+	message = FlexSendMessage(alt_text = "hello",contents= Carousel)
 	return message
