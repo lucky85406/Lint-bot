ec870b964436bd5a5dcc555519fbd955bdae0e67
陳柏任 <lucky85406@gmail.com>
2020-01-08 15:13:08 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -12,11 +12,13 @@ def go(url):
 	print(c)
 	print('----------------')
 	for x in c:
-		if x.split('-')[0] == url and x.split('-')[6] == '5':
-			print(c)
-			print('----------------')	
-			print(x)
+		if x.split('-')[6] == '5':
+			for y in c:
+				if x.split('-')[0] == url:
+					c.remove(x) 
+			break
 
+	print(c)
 
 	with open('user.csv','a',newline='')as cfile:
 			fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
