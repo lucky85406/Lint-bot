5a74df5404d1f1535d650cfdd513cfafab3ee3fd
陳柏任 <lucky85406@gmail.com>
2020-01-08 13:36:56 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -8,13 +8,13 @@ def go(url):
 		rows = csv.DictReader(csvfile)
 		print(type(rows))
 		for row in rows:
+			print(row)
 			c.append(row['ID']+"-"+row['D1']+'-'+row['D2']+'-'+row['D3']+'-'+row['D4']+'-'+row['D5']+"-"+row['Ver'])
 
 	for x in c:
 		if x.split('-')[6] == '5':
 			c.remove(x)
 
-	print(c)
 	with open('user.csv','a',newline='')as cfile:
 			fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
 
