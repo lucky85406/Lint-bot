44fa0e610368146f6f93ef29e8785ce0ba0e2896
lucky85406 <lucky85406@gmail.com>
2020-03-09 20:15:03 +0800

    OK

--- modelCode.py
+++ modelCode.py
@@ -397,66 +397,69 @@ def tenMod(img,dset):
 	contents = []
 	dlen = len(imglist)
 	print("圖片數量",dlen,sep=":")
-	for x in range(0,dlen):
-		contents.append(BubbleContainer(
-				hero = ImageComponent(
-							url = imglist[x],
-							size = 'full',
-							align = 'center',
-							aspect_ratio = '13:13',
-							aspect_mode = 'cover'
-				),
-				body = BoxComponent(
-							layout = 'vertical',
-							contents = [
-								TextComponent(
-									margin = 'md',
-									text = d[imglist[x]],
-									weight = 'bold',
-									size = 'md',
-									align = 'center'
-								),
-								TextComponent(
-									margin = 'md',
-									text = "第一段",
-									weight = 'bold',
-									size = 'xs',
-									align = 'center'
-								),
-								SeparatorComponent(
-									margin = 'xl',
-									color = '#0000FF'
-								),
-								BoxComponent(
-									margin = 'md',
-									layout = 'horizontal',
-									spacing = 'xs',
-									contents = [
-										ButtonComponent(
-											style='link',
-											color='#84C1FF',
-											flex = 1,
-											height='sm',
-											action=MessageAction(label="加入最愛",text='MyLove:'+d[imglist[x]])
-										),
-										ButtonComponent(
-											margin = 'xxl',
-											style='link',
-											color='#84C1FF',
-											flex = 1,
-											height='sm',
-											action=MessageAction(label="還沒想到",text='還沒想到')
-										)
-									]
-								)
-							]
+	if dlen > 0:
+		for x in range(0,dlen):
+			contents.append(BubbleContainer(
+					hero = ImageComponent(
+								url = imglist[x],
+								size = 'full',
+								align = 'center',
+								aspect_ratio = '13:13',
+								aspect_mode = 'cover'
+					),
+					body = BoxComponent(
+								layout = 'vertical',
+								contents = [
+									TextComponent(
+										margin = 'md',
+										text = d[imglist[x]],
+										weight = 'bold',
+										size = 'md',
+										align = 'center'
+									),
+									TextComponent(
+										margin = 'md',
+										text = "第一段",
+										weight = 'bold',
+										size = 'xs',
+										align = 'center'
+									),
+									SeparatorComponent(
+										margin = 'xl',
+										color = '#0000FF'
+									),
+									BoxComponent(
+										margin = 'md',
+										layout = 'horizontal',
+										spacing = 'xs',
+										contents = [
+											ButtonComponent(
+												style='link',
+												color='#84C1FF',
+												flex = 1,
+												height='sm',
+												action=MessageAction(label="加入最愛",text='MyLove:'+d[imglist[x]])
+											),
+											ButtonComponent(
+												margin = 'xxl',
+												style='link',
+												color='#84C1FF',
+												flex = 1,
+												height='sm',
+												action=MessageAction(label="還沒想到",text='還沒想到')
+											)
+										]
+									)
+								]
+					)
 				)
 			)
+		Carousel = CarouselContainer(
+			contents = contents
 		)
-	Carousel = CarouselContainer(
-		contents = contents
-	)
-	message = FlexSendMessage(alt_text = "hello",contents= Carousel)
+		message = FlexSendMessage(alt_text = "hello",contents= Carousel)
+	else:
+		message = TextSendMessage(TextSendMessage(text='Sorry，無此選項'+chr(0x100010)))
 	return message
 
 #顯示單張圖片
