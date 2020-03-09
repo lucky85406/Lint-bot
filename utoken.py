9149794683602caa1911adb6c7393792771e7057
lucky85406 <lucky85406@gmail.com>
2020-03-09 23:14:07 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -11,7 +11,7 @@ def go(url):
 		rows = csv.DictReader(csvfile)
 		for row in rows:
 			c.append(row)
-	
+
 	if len(c):
 		for x in c:
 			if x['ID'] == url:
@@ -34,7 +34,7 @@ def go(url):
 			writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 			writer.writeheader()
-			
+
 			writer.writerow({'ID':url,'D1':"",'D2':"",'D3':"",'D4':"",'D5':"",'Ver':"0"})
 
 def inMes(url,x):
@@ -44,7 +44,7 @@ def inMes(url,x):
 	with open('user.csv',newline='') as csvfile:
 		rows = csv.DictReader(csvfile)
 		for row in rows:
-			if row['ID'] == url and row['Ver'] == "0" and row['D1'] == "":
+			if row['ID'] == url and row['Ver'] == "0":
 				con = row['ID']
 				Dlist[0] = x
 				Dlist[1] = row['D2']
@@ -52,34 +52,34 @@ def inMes(url,x):
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
 				ver = "1"
-			elif row['ID'] == url and row['Ver'] == "1" and row['D2'] == "":
+			elif row['ID'] == url and row['Ver'] == "1":
 				Dlist[0] = row['D1']
 				Dlist[1] = x
 				Dlist[2] = row['D3']
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
 				ver = "2"
-			elif row['ID'] == url and row['Ver'] == "2" and row['D3'] == "":
+			elif row['ID'] == url and row['Ver'] == "2":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = x
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
 				ver = "3"
-			elif row['ID'] == url and row['Ver'] == "3" and row['D4'] == "":
+			elif row['ID'] == url and row['Ver'] == "3":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = row['D3']
 				Dlist[3] = x
 				Dlist[4] = row['D5']
 				ver = "4"
-			elif row['ID'] == url and row['Ver'] == "4" and row['D5'] == "":
+			elif row['ID'] == url and row['Ver'] == "4":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = row['D3']
 				Dlist[3] = row['D4']
 				Dlist[4] = x
-				ver = "5"		
+				ver = "5"
 
 	with open('user.csv','a',newline='')as cfile:
 			fieldn = ['ID','D1','D2','D3','D4','D5','Ver']
@@ -87,9 +87,9 @@ def inMes(url,x):
 			writer = csv.DictWriter(cfile,fieldnames=fieldn)
 
 			if con != url:
-				writer.writerow({'ID':url,'D1':"",'D2':"",'D3':"",'D4':"",'D5':"",'Ver':"0"})	
+				writer.writerow({'ID':url,'D1':"",'D2':"",'D3':"",'D4':"",'D5':"",'Ver':"0"})
 			else:
-				writer.writerow({'ID':url,'D1':Dlist[0],'D2':Dlist[1],'D3':Dlist[2],'D4':Dlist[3],'D5':Dlist[4],'Ver':ver})		
+				writer.writerow({'ID':url,'D1':Dlist[0],'D2':Dlist[1],'D3':Dlist[2],'D4':Dlist[3],'D5':Dlist[4],'Ver':ver})
 
 def showMes(url):
 	a = "1"
@@ -99,4 +99,3 @@ def showMes(url):
 			if row['ID'] == url and row['Ver'] == '5':
 				return "{}{}{}{}{}".format(row['D1'],row['D2'],row['D3'],row['D4'],row['D5'])
 	return a
-
