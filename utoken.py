b1b4ac2f6f75dde3eb301439d4a5706bfc89e6ac
lucky85406 <lucky85406@gmail.com>
2020-03-10 00:36:22 +0800

    OK

--- utoken.py
+++ utoken.py
@@ -61,45 +61,45 @@ def level(x):
 def inMes(url,x):
 	con = url
 	Dlist = [""]*5
-	level = level(x)
+	lev = level(x)
 	with open('user.csv',newline='') as csvfile:
 		rows = csv.DictReader(csvfile)
 		for row in rows:
-			if row['ID'] == url and level == "1":
+			if row['ID'] == url and lev == "1":
 				Dlist[0] = x
 				Dlist[1] = row['D2']
 				Dlist[2] = row['D3']
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
-				level = ""
-			elif row['ID'] == url and level == "2":
+				lev = ""
+			elif row['ID'] == url and lev == "2":
 				Dlist[0] = row['D1']
 				Dlist[1] = x
 				Dlist[2] = row['D3']
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
-				level = ""
-			elif row['ID'] == url and level == "3":
+				lev = ""
+			elif row['ID'] == url and lev == "3":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = x
 				Dlist[3] = row['D4']
 				Dlist[4] = row['D5']
-				level = ""
-			elif row['ID'] == url and level == "4":
+				lev = ""
+			elif row['ID'] == url and lev == "4":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = row['D3']
 				Dlist[3] = x
 				Dlist[4] = row['D5']
-				level = ""
-			elif row['ID'] == url and level == "5":
+				lev = ""
+			elif row['ID'] == url and lev == "5":
 				Dlist[0] = row['D1']
 				Dlist[1] = row['D2']
 				Dlist[2] = row['D3']
 				Dlist[3] = row['D4']
 				Dlist[4] = x
-				level = ""
+				lev = ""
 
 	with open('user.csv','a',newline='')as cfile:
 			fieldn = ['ID','D1','D2','D3','D4','D5']
