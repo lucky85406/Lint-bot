43c77bd825100e495f28c97677bf0f30d637929b
lucky85406 <lucky85406@gmail.com>
2020-03-15 17:07:33 +0800

    OK

--- googleSheet.py
+++ googleSheet.py
@@ -4,22 +4,26 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials as SAC
 
 def gmes(txt):
-	if txt != "":
-		GDriveJSON = 'googleToken.json'
-		GSpreadSheet = ' P_UpLoadTest'
 
-		while True:
-			try:
-				scope = ['https://spreadsheets.google.com/feeds']
-				key = SAC.from_json_keyfile_name(GDriveJSON,scope)
-				gc = gspread.authorize(key)
-				worksheet = gc.open(GSpreadSheet).sheet1
-			except Exception as e:
-				print('無法連線Google試算表',e)
-				sys.exit(1)
-			textt = ""
-			textt+= text
-			if textt != "":
-				worksheet.append_row((datatime.datetime.now(),textt))
-				print('新增一列資料到試算表OK',GSpreadSheet)
-				return textt
+	auth_json_path = 'googleToken.json' #由剛剛建立出的憑證，放置相同目錄以供引入
+	gss_scopes = ['https://spreadsheets.google.com/feeds'] #我們想要取用的範圍
+
+
+	
+	def auth_gss_client(path, scopes):
+		credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scopes)
+		return gspread.authorize(credentials)
+
+	gss_client = auth_gss_client(auth_json_path, gss_scopes) #呼叫我們的函式
+
+
+	#從剛剛建立的sheet，把網址中 https://docs.google.com/spreadsheets/d/〔key〕/edit 的 〔key〕的值代入
+	spreadsheet_key_path = '1vhiAa6idyIwIkVVZdTXzhAHKclf9lvb5j4PXhsodWXM'
+
+	#我們透過open_by_key這個method來開啟sheet
+	sheet = gss_client.open_by_key(spreadsheet_key_path).sheet1
+	#單純取出時間稍後塞入sheet
+	now = datetime.now()
+	time = now.strftime("%Y/%m/%d-%H:%M:%S")
+	#透過insert_row寫入值 第二行塞入時間,abc,123的值
+	sheet.insert_row([time,txt],2)
