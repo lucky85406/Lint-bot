575f63d7d1b38ebae95294c3791566b8fd13da99
lucky85406 <lucky85406@gmail.com>
2020-03-20 00:14:48 +0800

    OK

--- googleSheet.py
+++ googleSheet.py
@@ -37,4 +37,4 @@ def kmes(user,love):
 def gmes():
 	dict = {'love':[i for i in token().col_values(3)]}
 	if '柏任' in token().col_values(2):
-		print(token().col_values(2))
+		print(token().col_values(3))
