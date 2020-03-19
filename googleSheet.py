9e075b51901b55512900663566b4f39932d9d185
lucky85406 <lucky85406@gmail.com>
2020-03-20 00:50:50 +0800

    OK

--- googleSheet.py
+++ googleSheet.py
@@ -40,6 +40,6 @@ def gmes(user):
 	if user in token().col_values(2):
 		data.append(token().col_values(3))
 	for x in data:
-		if x != "love":
+		if x is not "love":
 			dict[user] = x
 	print(dict)
